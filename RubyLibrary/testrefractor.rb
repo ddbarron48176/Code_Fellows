47a3cd185eb3e173572eb06889a3d6eefcd54206
David Barron <ddbarron48176@gmail.com>
2013-09-23 17:44:02 -0400

    Update

--- RubyLibrary/testrefractor.rb
+++ RubyLibrary/testrefractor.rb
@@ -1,44 +1,46 @@
 # Library creates initializes shelves and book array.
+
+# Use object-oriented Ruby to model a public library (w/ three classes: Library, Shelf, & Book).
+# The library should be aware of a number of shelves. Each shelf should know what books it contains.
+# Make the book object have "enshelf" and "unshelf" methods that control what shelf the book is sitting on.
+# The library should have a method to report all books it contains.
+# Note: this should *not* be a Rails app - just a single file with three classes (plus commands at the bottom showing it works) is all that is needed.
+
 class Library
+  attr_reader :shelves
 
   def initialize()
     @shelves = []
-      end
+  end
 
   def add_shelf(shelf)
     @shelves << shelf
   end
 
-  def get_shelves
-    @shelves
-  end
-
-  def display_all_books    
-    if $all_books.empty?
+  def display_all_books
+    if all_books.empty?
       puts "There is no books in the library"
     else
-      puts $all_books
+      puts all_book_titles
     end
   end
 
+  private
+
+  def all_book_titles
+    all_books.collect(&:title)
+  end
+
+  def all_books
+    @shelves.collect(&:books).flatten
+  end
 end
 
-class Shelf < Library
-  attr_accessor :add_book_to_shelf, :remove_book_from_shelf
+class Shelf
+  attr_reader :books
 
-  def initialize(shelf_number)
-    @num = shelf_number
+  def initialize
     @books = []
-    $shelf_count += 1
-  end
-
-  def get_books
-    puts "Books contained on shelf [" + self.get_shelf_number.to_s + "]: " + $all_books.to_s + ""
-    @books.each { |a| puts a }
-  end
-
-  def get_shelf_number
-    @num
   end
 
   def add_book_to_shelf(book)
@@ -46,42 +48,35 @@ class Shelf < Library
   end
 
   def remove_book_from_shelf()
-   @books.pop()
+    @books.pop()
   end
 end
 
 class Book < Shelf
-  attr_accessor :get_book_title, :get_book_count
+  attr_accessor :title
 
   def initialize(title)
-    @book_title = title
-    $all_books = []
-    $all_books << title
-    
+    @title = title
   end
 
   def get_book_title
-    @book_title
+    @title
   end
 
   def enshelf(shelf)
-    shelf.add_book_to_shelf(self) 
+    shelf.add_book_to_shelf(self)
+      
   end
 
   def unshelf(shelf)
-    shelf.remove_book_from_shelf() 
+    shelf.remove_book_from_shelf()
   end
-  $book_count = 0
-  $shelf_count = 0
 end
 
-
-$lib = Library.new() # Creating library
-s1 = Shelf.new(1) # Create shelf 1
-b1 = Book.new("The Ruby Chronicles") # Create new book
-s1.get_books
-$lib.display_all_books
-
-
-
-
+library   = Library.new() # Creating library
+shelf     = Shelf.new
+book_1    = Book.new("The Ruby Chronicles") # Create new book
+book_1.enshelf(shelf)
+shelf.books
+library.add_shelf shelf
+library.display_all_books
